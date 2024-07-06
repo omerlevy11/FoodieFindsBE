a3d45771f067c1284b8221cff845c749319454b0
omerlevy11 <omerlevy111@gmail.com>
2024-07-06 15:21:01 +0300

    added routes2

--- src/index.ts
+++ src/index.ts
@@ -10,31 +10,31 @@ import userPostRoute from "./routes/posts.route"
 
 
 const initApp = (): Promise<Express> => {
-  const promise = new Promise<Express>((resolve) => {
-    const db = mongoose.connection;
-    db.once("open", () => console.log("Connected to Database"));
-    db.on("error", (error) => console.error(error));
-    const url ="mongodb+srv://noambz:OrdMLN4rK4CTuJGA@mycluster.sqyxtxd.mongodb.net/?appName=MyCluster"// process.env.DB_URL;
-    mongoose.connect(url!).then(() => {
-      const app = express();
-      app.use(bodyParser.json());
-      app.use(bodyParser.urlencoded({ extended: true }));
-      app.use((req, res, next) => {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Methods", "*");
-        res.header("Access-Control-Allow-Headers", "*");
-        res.header("Access-Control-Allow-Credentials", "true");
-        next();
-      })
-      app.use("/user", userRoute);
-      app.use("/userPost",userPostRoute)
-      app.use("/auth", authRoute);
-      app.use("/public", express.static("public"));
-      app.use("/file", fileRoute);
-      resolve(app);
+    const promise = new Promise<Express>((resolve) => {
+        const db = mongoose.connection;
+        db.once("open", () => console.log("Connected to Database"));
+        db.on("error", (error) => console.error(error));
+        const url = "mongodb+srv://noambz:OrdMLN4rK4CTuJGA@mycluster.sqyxtxd.mongodb.net/?appName=MyCluster"// process.env.DB_URL;
+        mongoose.connect(url!).then(() => {
+            const app = express();
+            app.use(bodyParser.json());
+            app.use(bodyParser.urlencoded({ extended: true }));
+            app.use((req, res, next) => {
+                res.header("Access-Control-Allow-Origin", "*");
+                res.header("Access-Control-Allow-Methods", "*");
+                res.header("Access-Control-Allow-Headers", "*");
+                res.header("Access-Control-Allow-Credentials", "true");
+                next();
+            })
+            app.use("/user", userRoute);
+            app.use("/userPost", userPostRoute)
+            app.use("/auth", authRoute);
+            app.use("/public", express.static("public"));
+            app.use("/file", fileRoute);
+            resolve(app);
+        });
     });
-  });
-  return promise;
+    return promise;
 };
 
 export default initApp;
